ce402011f8404588b44cc56f2e40af3b7001c6a1
thehulke <lior.gerstein@gmail.com>
2015-10-10 12:17:31 +0300

    adding sessions

--- client/app/controllers/loginController.js
+++ client/app/controllers/loginController.js
@@ -2,25 +2,36 @@
   console.log('login controller');
 
   angular.module('chairGame')
-    .controller('LoginCtrl', ['roomService', '$state', LoginCtrl]);
+    .controller('LoginCtrl', ['roomService', '$state', '$cookies', LoginCtrl]);
 
-  function LoginCtrl(roomService, $state) {
+  function LoginCtrl(roomService, $state, $cookies) {
 
 
     this.playAsGuest = function() {
       var freeRoom = roomService.findOneEmpty();
       var playerName = this.playerName;
-      var gameId;
+      var gameId, playerId;
 
-      if(!freeRoom) {
-        gameId = roomService.create({gameStart: false, players: [{"name" : playerName}]});
+      if (!freeRoom) {
+        playerId = Random.id();
+        gameId = roomService.create({
+          gameStart: false,
+          players: [{
+            _id: playerId,
+            name: playerName
+          }]
+        });
+
       } else {
         gameId = freeRoom._id;
         roomService.addPlayer(freeRoom._id, {
           name: playerName
         });
+
       }
-      $state.go('game',{id: gameId});
+      $state.go('game', {
+        id: gameId
+      });
     };
   }
 
--- client/app/services/roomService.js
+++ client/app/services/roomService.js
@@ -29,6 +29,8 @@
     }
 
     function create(roomDetails) {
+      Session.set('currentPlayer',roomDetails.players[0]._id);
+      console.log(Session.get('currentPlayer'));
       return Room.insert(roomDetails);
     }
 
@@ -37,16 +39,14 @@
       var room = Room.findOne({_id:roomId});
       var player = playerDetails;
       player._id = Random.id();
-
       if(room.players.length >= 4) {
         room.gameStart = true;
       }
-
       Session.set('currentPlayer',player._id);
+      console.log(Session.get('currentPlayer'));
       room.players.push(player);
 
       return Room.update(roomId, room);
-
     }
 
   }
